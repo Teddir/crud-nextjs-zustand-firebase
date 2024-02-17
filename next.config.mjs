b597bd7494ff8db52e645d9cac5a8c121947d7f9
Teddir <ferryaja20000@gmail.com>
2024-02-17 14:15:29 +0700

    fix:  next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  compiler: {
+    removeConsole: production ? { exclude: ["error"] } : false,
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGE_SENDER_ID: process.env.MESSAGE_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+  },
 };
 
 export default nextConfig;
